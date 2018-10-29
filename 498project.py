9d0664d6e5c8ad2f3e8c2695328ac6a45fdd9619
timklabjan <tklabz28@gmail.com>
2018-10-29 15:09:11 -0500

    Update 498project.py

--- 498project.py
+++ 498project.py
@@ -32,3 +32,4 @@ model = model.fit(x_train,y_train)
 #get predictions and accuracy
 pred = model.predict(x_test)
 print(accuracy_score(y_test,pred))
+print(accuracy_score(y_test,np.random.choice(['0','1','2','3','4','5','6','7','8','9'],size=len(y_test),replace=True)))
