6cd06676e0968d8140cba14fecc27600096df072
timklabjan <tklabz28@gmail.com>
2018-10-30 19:20:55 -0500

    Update 498project.py

--- 498project.py
+++ 498project.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 from sklearn.metrics import accuracy_score
-
+import numpy as np
 #read in data
 data = pd.read_csv("/Users/timklabjan/Downloads/Task3_content_topic_prediction/Topic_prediction_data_train.txt",sep='\t',header=None)
 data.columns = ['text','uid','type','timestamp']
@@ -21,15 +21,26 @@ v = CountVectorizer()
 bow = pd.DataFrame(v.fit_transform(corpus).todense())
 bow['label'] = data['label'].astype(int).astype(str)
 bow = bow[bow['label'].notnull()]
+inputs = bow[bow.columns.tolist()[:-1]]
+labels = bow['label']
+del(bow)
 
-#train/test data split
-x_train, x_test, y_train, y_test = train_test_split(bow[bow.columns.tolist()[:-1]],bow['label'],test_size=0.25,random_state=387)
+#k fold cross validation
+kf = KFold(n_splits=3,random_state=387)
+kf.get_n_splits(inputs)
 
-#train model
-model = LogisticRegression()
-model = model.fit(x_train,y_train)
+accuracies = []
 
-#get predictions and accuracy
-pred = model.predict(x_test)
-print(accuracy_score(y_test,pred))
+for train_index, test_index in kf.split(inputs):
+    print("training model")
+    X_train, X_test = inputs.iloc[train_index], inputs.iloc[test_index]
+    y_train, y_test = labels[train_index], labels[test_index]
+    #train model
+    model = LogisticRegression(random_state=387)
+    model = model.fit(X_train,y_train)
+    # get predictions and accuracy
+    pred = model.predict(X_test)
+    accuracies.append(accuracy_score(y_test, pred))
+
+print(sum(accuracies)/3.0)
 print(accuracy_score(y_test,np.random.choice(['0','1','2','3','4','5','6','7','8','9'],size=len(y_test),replace=True)))
